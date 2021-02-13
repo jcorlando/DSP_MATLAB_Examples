7c64a1d88bcc4503ca92eb8d7767842e903d27fa
Juan Orlando <jcorlandomc@yahoo.com>
2021-02-12 21:57:25 -0800

    02/12/2021 09:57 pm

--- assignment_3.m
+++ assignment_3.m
@@ -3,14 +3,15 @@ clear all
 close all
 
 % fft example
-n = [0:100];
-% x = ( 3*sin( (((2*pi).*n)/7) + (pi/4) ) );
-x = sin((pi.*n)/14);
-stem(x);        %  This is to plot the signal and check if it is periodic
-% N = length(x);
-% c = fft(x)/N;
-% subplot(1, 2, 1)
-% stem(0:9, abs(c) );
-% subplot(1, 2, 2)
-% stem(0:9, angle(c) );
+n = [0:9];
+x = [1, 1, 1, 0, 0, 0, 0, 0, 1, 1];
+% x = sin( ((2*pi)*(3/10)).*n );
+% stem(n, x);               % This is to plot the signal and check if it is periodic
+N = length(x);
+c = fft(x)/N;
+stem(n, c);               % This is to plot the DTFS
+subplot(1, 2, 1)
+stem(n, abs(c) );
+subplot(1, 2, 2)
+stem(n, angle(c) );
 
