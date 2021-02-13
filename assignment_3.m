137e3f49174d42518865207783d0db822b8a353c
Juan Orlando <jcorlandomc@yahoo.com>
2021-02-13 02:29:55 -0800

    02/13/2021 02:29 am

--- assignment_3.m
+++ assignment_3.m
@@ -3,10 +3,13 @@ clear all
 close all
 
 % fft example
-n = [-49:1:50];
-x = ((1/2).^abs(n)).*(cos(pi*(n-1)/8));
-omega = linspace(-pi, pi, 60);
+n = -1:1:1;
+x = [1 1 1];
+% x = ((1/2).^abs(n)).*(cos(pi*(n-1)/8));
+omega = -pi:(pi/1000):pi;
 N = length(x);
+X = exp(-j*omega'*n)*(x');
+
 
 % Analysis Formula
 % Find the Fourier Transform of signal "for discrete series divide by /N" 
@@ -14,13 +17,25 @@ N = length(x);
 % Synthesis Formula
 % This is to find the inverse Fourier Transform "for discrete series multiply by *N"
 % x = ifft(c)*N;
-
-
-subplot(2, 2, [1,2])        % This is a plot of the original signal
-stem(n, x, 'b');            % This is a plot of the original signal
-subplot(2, 2, 3)            % This is the magnitude plot
-plot(abs(c), 'r' );         % This is the magnitude plot
-subplot(2, 2, 4)            % This is the phase plot
-plot(angle(c), 'g' );       % This is the phase plot
 % stem(n, c);                 % This is to plot the DTFS
+
+
+subplot(2, 2, [1,2])                    % This is a plot of the original signal
+stem(n, x, 'b');                        % This is a plot of the original signal
+xlabel('n')
+ylabel('x[n]')
+title('x[n]')
+
+subplot(2, 2, 3)                        % This is the magnitude plot
+plot( (omega/pi), abs(X), 'r' );        % This is the magnitude plot
+title('Magnitude response')
+xlabel('w/pi')
+ylabel('|X(e^jw)|')
+
+subplot(2, 2, 4)                        % This is the phase plot
+plot((omega/pi), angle(X), 'g' );       % This is the phase plot
+title('Phase response')
+xlabel('w/pi')
+ylabel('<X(e^jw)')
+
 
