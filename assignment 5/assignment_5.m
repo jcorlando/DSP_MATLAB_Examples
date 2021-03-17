b643153e97d62e3cdd75f857f47bc069a0b1f382
Juan Orlando <jcorlandomc@yahoo.com>
2021-03-17 01:50:47 -0700

    03/17/2021 01:50 am

--- assignment 5/assignment_5.m
+++ assignment 5/assignment_5.m
@@ -25,13 +25,49 @@ close all
 
 
 % Problem #2 part e
-n = 0:100;
-x = 2*cos(((pi/2).*n) + pi/3);
-y = filter([0.19], [1 0 0.81], x);
-stem(x, 'filled');
-hold on;
-stem(y, 'filled');
-title('Input x[n] Blue and Steady State Response y[n] Orange');
+% n = 0:100;
+% x = 2*cos(((pi/2).*n) + pi/3);
+% y = filter([0.19], [1 0 0.81], x);
+% stem(x, 'filled');
+% hold on;
+% stem(y, 'filled');
+% title('Input x[n] Blue and Steady State Response y[n] Orange');
+
+
+% Problem # 6 Part (b)
+% n = -pi:pi/1000:pi;
+% b = [1 6 15 20 15 6 1]; %numerator coefficient of transfer function H(z)=[(1+z^-1)/2]^6
+% a = [64];               %denominator coefficient of transfer function H(z)
+% [h,w] = freqz(b, a, n);
+% plot(w/pi, unwrap(angle(h)));
+% title('phase plot of H(z)=[(1+Z^{-1})/2]^6');
+
+
+% Problem # 18 Part (a)
+% n = -pi:pi/1000:pi;
+% b = [2 0 3.125];
+% a = [1 -0.9 0.81];
+% [h, w] = freqz(b, a, n);
+% subplot(1, 2, 1);
+% plot( w/pi, (abs(h)) );
+% title('Magnitude Response of H(z)');
+% subplot(1, 2, 2);
+% plot( w/pi, unwrap(angle(h)) );
+% title('Phase Response of H(z)');
+
+
+% Problem # 18 Part (b)
+n = -pi:pi/1000:pi;
+b = [3.125 0 2];
+a = [1 -0.9 0.81];
+[h, w] = freqz(b, a, n);
+subplot(1, 2, 1);
+plot( w/pi, (abs(h)) );
+title('Magnitude Response of H_m_i_n(z)');
+subplot(1, 2, 2);
+plot( w/pi, unwrap(angle(h)) );
+title('Phase Response of H_m_i_n(z)');
+
 
 
 
