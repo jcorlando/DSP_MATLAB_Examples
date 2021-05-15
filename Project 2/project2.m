ee0bcfe5457006f2f5655642b2464698ea3df992
Juan Orlando <jcorlandomc@yahoo.com>
2021-05-15 12:12:17 -0700

    05/15/2021 12:12 pm

--- Project 2/project2.m
+++ Project 2/project2.m
@@ -21,46 +21,73 @@ fshift = (-n/2:(n/2)-1)*(fs/n); % <--- x axis frequency (Hz)
 yshift = fftshift(y);   % <--- shifted fft around 0 frequency
 % Discrete Fourier Transform of signal
 
-% Plots Of Frequency Spectrum
-% subplot(2,1,1);
-% plot(fshift, abs(yshift));
-% xlabel('Frequency (Hz)')
-% ylabel('||');
-% ylim([-7000 60000]);
-% xlim([-300 6000]);
-% title('Plot of Frequency Spectrum');
-% subplot(2,1,2);
-% plot(fshift, 10*log(abs(yshift)));
-% xlabel('Frequency (Hz)')
-% ylabel('Magnitude');
-% xlim([-300 6000]);
-% title('Corresponding Frequency Spectrum Plot Using Logarithmic Decibel Scaling (dB)');
-% End Plots Of Frequency Spectrum
-
-
-% Impulse Response of filter 1
-wc1 = 1800;
-wc2 = 2200;
-wn = [wc1 wc2]/(fs/2);
-N = 700;
-h = fir1(N, wn, 'stop');
-[H, w] = freqz(h, 1);
+% Plots Of Frequency Spectrum of Original Signal
 figure();
-subplot(2, 1, 1);
-plot(w*fs/(2*pi), 20*log10(abs(H)));
-title('Frequency Response');
-xlabel('frequency (Hz)');
-ylabel('Magnitude Response (dB)');
-axis([0, fs/2, -80, 5]);
-subplot(2, 1, 2);
-plot(w*fs/(2*pi), unwrap(angle(H)));
-xlabel('frequency (Hz)');
-ylabel('Phase Response (Radians)');
+subplot(2,1,1);
+plot(fshift, abs(yshift));
+xlabel('Frequency (Hz)')
+ylabel('Magnitude');
+ylim([-7000 60000]);
+xlim([-300 6000]);
+title('Plot of Frequency Spectrum');
+subplot(2,1,2);
+plot(fshift, 10*log(abs(yshift)));
+xlabel('Frequency (Hz)')
+ylabel('Magnitude');
+xlim([-300 6000]);
+title('Corresponding Frequency Spectrum Plot Using Logarithmic Decibel Scaling (dB)');
+% End Plots Of Frequency Spectrum of Original Signal
+
+% Create Filter # 1
+wc1 = 1106;
+wc2 = 1150;
+wn = [wc1 wc2]/(fs/2);
+N = 4000;
+h = fir1(N, wn, 'stop');
+% Create Filter # 1
+
+% Impulse Response of filter 1
+% [H, w] = freqz(h, 1);
+% figure();
+% subplot(2, 1, 1);
+% plot(w*fs/(2*pi), 20*log10(abs(H)));
+% title('Frequency Response');
+% xlabel('frequency (Hz)');
+% ylabel('Magnitude Response (dB)');
+% subplot(2, 1, 2);
+% plot(w*fs/(2*pi), unwrap(angle(H)));
+% xlabel('frequency (Hz)');
+% ylabel('Phase Response (Radians)');
 % Impulse Response of filter 1
 
+% Filter the Signal with filter # 1
+x = filter(h, 1, x);
+% x = filter(h2, 1, x);
+% Filter the Signal with filter # 1
 
+% DFT of signal of filtered signal after filter #1
+y = fft(x);
+n = length(x);     % <--- Number of samples in the signal
+fshift = (-n/2:(n/2)-1)*(fs/n); % <--- x axis frequency (Hz)
+yshift = fftshift(y);   % <--- shifted fft around 0 frequency
+% DFT of signal of filtered signal after filter #1
 
-
+% Plots Of Frequency Spectrum of Signal After Filter # 1
+figure();
+subplot(2,1,1);
+plot(fshift, abs(yshift));
+xlabel('Frequency (Hz)')
+ylabel('Magnitude');
+ylim([-7000 60000]);
+xlim([-300 6000]);
+title('Plot of Frequency Spectrum After Filter # 1');
+subplot(2,1,2);
+plot(fshift, 10*log(abs(yshift)));
+xlabel('Frequency (Hz)')
+ylabel('Magnitude');
+xlim([-300 6000]);
+title('Corresponding Frequency Spectrum Plot Using Logarithmic Decibel Scaling (dB)');
+% Plots Of Frequency Spectrum of Signal After Filter # 1
 
 
 
